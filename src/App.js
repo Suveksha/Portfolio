d9dbca782a5f87c2f4f56efb432b5276583e8de4
Suveksha Chettri <suvekshachettri@Suvekshas-MacBook-Air.local>
2022-03-16 02:03:49 +0530

    Day 1: Completed till the scrolling part.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Contact from "./components/contact/Contact";
+import Intro from "./components/intro/Intro";
+import Navbar from "./components/nav-bar/Navbar";
+import Portfolio from "./components/portfolio/Portfolio";
+import Test from "./components/testimonials/Test";
+import Works from "./components/works/Works";
+import "./App.scss"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+      <Navbar/>
+      <div className="sections">
+        <Intro/>
+        <Portfolio/>
+        <Works/>
+        <Test/>
+        <Contact/>
+      </div>
+      </>
     </div>
+   
   );
 }
 
---
+++ src/components/testimonials/Test.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import './Test.scss'
+export default function Test() {
+  return (
+    <div className='test'>Test</div>
+  )
+}
